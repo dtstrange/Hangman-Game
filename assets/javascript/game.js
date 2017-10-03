fae06c2b1d31d70da196537a6340731aa6734e06
dtstrange <dustintstrange@gmail.com>
2017-10-02 19:05:02 -0700

    added basic html, some css, and begginning of JS

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+var game = {
+	creatures : ["BASILISK", "CENTAUR", 
+				"CHIMERA", "DRAGON",
+				"GORGON", "GRIFFIN",
+				"HYDRA", "KRAKEN",
+				"MANTICORE", "MINOTAUR",
+				"PEGASUS", "PHOENIX",
+				"PSYCLOPS", "SAYTR"
+				]
+
+	
+	
+	
+
+ 		
+ 		
+ 	
+
+ }; //end game var
+
+var winCounter = 0;
+var loseCounter = 0;
+var numberOfTries = 15;
+
+document.onkeyup = fucntion(event){
+ 	var keyPress = event.key.toLowerCase()
+ }
+
+ console.log(event)
+
+ displayChoice: fucntion(){
+		document.getElementById("guesses").innerHTML = ("event")
+	}
+
