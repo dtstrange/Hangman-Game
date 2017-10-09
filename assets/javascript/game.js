c74bcfa1d833e3d853c93f81f1958d530b12ae0b
dtstrange <dustintstrange@gmail.com>
2017-10-08 19:05:01 -0700

    added loops and onkeyup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,99 @@
-var game = {
-	creatures : ["BASILISK", "CENTAUR", 
-				"CHIMERA", "DRAGON",
-				"GORGON", "GRIFFIN",
-				"HYDRA", "KRAKEN",
-				"MANTICORE", "MINOTAUR",
-				"PEGASUS", "PHOENIX",
-				"PSYCLOPS", "SAYTR"
-				]
+//array of words to be used
+var creatures = ["basilisk", "centaur",
+    "chimera", "dragon",
+    "gorgon", "griffin",
+    "hydra", "kraken",
+    "manticore", "minotaur",
+    "pegasus", "phoenix",
+    "psyclops", "saytr"
+]
 
-	
-	
-	
+//array of creatures decriptions
+var beastiary = [
+    "basilisk", 'Basilisk mythology describes this "king of serpents" as a reptilian creature with sometimes rooster-like qualities who is famous for its ability to kill with a single glance. Like its bird-lizard cousin, the Cockatrice, basilisks are said to be born from a union of serpents and roosters.',
 
- 		
- 		
- 	
+    "centaur", 'Centaurs are half-human, half-horse creatures in Greek mythology. They have the body of a horse and the torso, head and arms of a man. They were considered to be the children of Ixion, king of the Lapiths, and Nephele, a cloud made in the image of Hera. According to a different myth, however, they were all born from the union of a single Centaurus with the Magnesian mares.',
 
- }; //end game var
+    "chimera", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
 
+    "dragon", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "gorgon", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "griffin", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "hydra", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "kraken", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "manticore", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "minotaur", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "pegasus", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "phoenix", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "psyclops", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+    "saytr", "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+
+] //end beastiary object
+
+var arrLength = creatures.length;
+
+
+//counters
 var winCounter = 0;
 var loseCounter = 0;
 var numberOfTries = 15;
+//selects word at random from array
+var curWord = creatures[Math.floor(Math.random() * arrLength)];
+var splitword = curWord.split("");
+var creatureWordLength = curWord.length;
+//gathers html element for manipulation
+var targetsolinfo = document.getElementById("sol-info");
+var targetpuzzleWord = document.getElementById("puzzleWord")
+var targetwin = document.getElementById("win");
+var targetlose = document.getElementById("lose");
+//log to make sure word is being selected at random
+console.log(curWord)
 
-document.onkeyup = fucntion(event){
- 	var keyPress = event.key.toLowerCase()
- }
 
- console.log(event)
 
- displayChoice: fucntion(){
-		document.getElementById("guesses").innerHTML = ("event")
-	}
+
+var letterInWord = false;
+//creates placen holder for word
+var placeHolder = "";
+for (var i = 0; i < creatureWordLength; i++) {
+    placeHolder += "_";
+}
+targetpuzzleWord.innerHTML = placeHolder;
+
+console.log("yo place: ", placeHolder);
+
+
+
+
+
+
+//captures key stroke
+document.onkeyup = function(event) {
+    var userGuess = event.key.toLowerCase();
+    console.log(userGuess)
+
+    //checks keystroke against word
+    for (var r = 0; r < curWord.length; r++) {
+        if (userGuess === curWord[r]) {
+            placeHolder[r] = userGuess;
+            console.log(placeHolder);
+        }
+    }   
+        
+        
+        
+};
+
+
+
 
